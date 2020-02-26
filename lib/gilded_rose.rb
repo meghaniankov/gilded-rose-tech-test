6e6fde0c8ab9096d280a8144305667e0ec230882
Meghan <meghan.e.mccue@gmail.com>
2020-02-26 12:42:10 +0000

    Refactor #update_quality to run individual #update methods

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -2,57 +2,10 @@ class GildedRose
 
   def update_quality(items)
     items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
-
-      # update_aged_brie(item) if item.name == 'Aged Brie'
-      # update_backstage_passes(item) if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-      # update_sulfuras(item) if item.name == 'Sulfuras, Hand of Ragnaros'
-      # update_other(item) if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert' && item.name != 'Sulfuras, Hand of Ragnaros'
-
+      update_aged_brie(item) if item.name == 'Aged Brie'
+      update_backstage_passes(item) if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+      update_sulfuras(item) if item.name == 'Sulfuras, Hand of Ragnaros'
+      update_other(item) if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert' && item.name != 'Sulfuras, Hand of Ragnaros'
     end
   end
 
